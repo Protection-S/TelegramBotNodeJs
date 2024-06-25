a83ec45779dedfc1df31c9ff9998e14d80f94624
maxim <lalka.roka.1999@gmail.com>
2024-06-25 12:14:20 +1000

    обновленный бот

--- app.js
+++ app.js
@@ -4,8 +4,42 @@ const token = '7432819616:AAFcIcDQ8_kC5HgFDRHJnDRt3araladJ92o';
 
 const bot = new TelegramBot(token, {polling: true});
 
-bot.on('message', (msg) => {
-  const chatId = msg.chat.id;
+const keyboard = [
+  [{ text: '/help' }],
+  [{ text: '/site' }],
+  [{ text: '/creator' }],
+];
 
-  bot.sendMessage(chatId, 'Привет, октагон!');
+bot.onText(/\/start/, (msg) => {
+  const chatId = msg.chat.id;
+  bot.sendMessage(chatId, 'Приветствую! Выберите команду:', {
+    reply_markup: {
+      keyboard: keyboard,
+      resize_keyboard: true,
+      one_time_keyboard: true,
+    },
+  });
+});
+
+bot.onText(/\/help/, (msg) => {
+  const chatId = msg.chat.id;
+  const helpMessage = `
+  Список доступных команд:
+  /help - возвращает список команд с их описанием
+  /site - отправляет в чат ссылку на сайт октагона
+  /creator - отправляет в чат мое ФИО
+  `;
+  bot.sendMessage(chatId, helpMessage);
+});
+
+bot.onText(/\/site/, (msg) => {
+  const chatId = msg.chat.id;
+  const siteUrl = 'https://students.forus.ru/';
+  bot.sendMessage(chatId, `Сайт октагона: ${siteUrl}`);
+});
+
+bot.onText(/\/creator/, (msg) => {
+  const chatId = msg.chat.id;
+  const creatorInfo = 'Банников Максим Бесикович';
+  bot.sendMessage(chatId, `Его величество: ${creatorInfo}`);
 });
