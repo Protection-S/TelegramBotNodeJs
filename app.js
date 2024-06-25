a5735586b8b5b59dfc1bf58a511898f3b24e4dcd
maxim <lalka.roka.1999@gmail.com>
2024-06-25 13:03:30 +1000

    бот с использованием базы данных

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
 const TelegramBot = require('node-telegram-bot-api');
+const mysql = require("mysql2");
 
 const token = '7432819616:AAFcIcDQ8_kC5HgFDRHJnDRt3araladJ92o';
 
 const bot = new TelegramBot(token, {polling: true});
 
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'ChatBotTests'
+});
+
+connection.connect(err=>{
+  if(err){
+      console.error('Error connection to the DB'. err.stack);
+      return;
+  }
+
+  console.log('Connected to the DB')
+});
+
+
+
 const keyboard = [
   [{ text: '/help' }],
   [{ text: '/site' }],
-  [{ text: '/creator' }],
+  [{ text: '/creator' }]
 ];
 
 bot.onText(/\/start/, (msg) => {
@@ -27,6 +46,9 @@ bot.onText(/\/help/, (msg) => {
   Список доступных команд:
   /site - отправляет в чат ссылку на сайт октагона
   /creator - отправляет в чат мое ФИО
+  /randomItem - возвращает случайный предмет из БД
+  /deleteItem (id объекта, скобки не писать) - удаляет предмет из БД по ID
+  /getItemByID (id объекта, скобки не писать)- возвращает предмет из БД по ID
   `;
   bot.sendMessage(chatId, helpMessage);
 });
@@ -41,4 +63,52 @@ bot.onText(/\/creator/, (msg) => {
   const chatId = msg.chat.id;
   const creatorInfo = 'Банников Максим Бесикович';
   bot.sendMessage(chatId, `Его величество: ${creatorInfo}`);
+});
+
+bot.onText(/\/randomItem/, (msg) => {
+  const chatId = msg.chat.id;
+  connection.query('SELECT * FROM items ORDER BY RAND() LIMIT 1', (err, results) => {
+    if (err) {
+      bot.sendMessage(chatId, 'Ошибка при получении случайного предмета.');
+      console.error(err);
+    } else if (results.length > 0) {
+      const item = results[0];
+      bot.sendMessage(chatId, `(${item.id}) - ${item.name}: ${item.description}`);
+    } else {
+      bot.sendMessage(chatId, 'В базе данных нет предметов.');
+    }
+  });
+});
+
+bot.onText(/\/deleteItem (.+)/, (msg, match) => {
+  const chatId = msg.chat.id;
+  const itemId = match[1];
+
+  connection.query('DELETE FROM items WHERE id = ?', [itemId], (err, results) => {
+    if (err) {
+      bot.sendMessage(chatId, 'Ошибка при удалении предмета.');
+      console.error(err);
+    } else if (results.affectedRows > 0) {
+      bot.sendMessage(chatId, 'Предмет успешно удален.');
+    } else {
+      bot.sendMessage(chatId, 'Ошибка: предмет не найден.');
+    }
+  });
+});
+
+bot.onText(/\/getItemByID (.+)/, (msg, match) => {
+  const chatId = msg.chat.id;
+  const itemId = match[1];
+
+  connection.query('SELECT * FROM items WHERE id = ?', [itemId], (err, results) => {
+    if (err) {
+      bot.sendMessage(chatId, 'Ошибка при получении предмета.');
+      console.error(err);
+    } else if (results.length > 0) {
+      const item = results[0];
+      bot.sendMessage(chatId, `(${item.id}) - ${item.name}: ${item.description}`);
+    } else {
+      bot.sendMessage(chatId, 'Ошибка: предмет не найден.');
+    }
+  });
 });
